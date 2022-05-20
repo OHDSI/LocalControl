748941aac14eee0ca5056f855bc486de9aa5daf2
ChristopheLambert <christophe.g.lambert@gmail.com>
2022-05-20 11:10:30 -0600

    Removed extras folder that CRAN was generating warnings about. Its purpose was to hold the pdf version of the manual, but this is not required and takes space.

--- extras/PackageMaintenance.R
+++
@@ -1,8 +0,0 @@
-# Format and check code:
-OhdsiRTools::formatRFolder()
-OhdsiRTools::checkUsagePackage("LocalControl")
-OhdsiRTools::updateCopyrightYearFolder()
-
-# Create manual and vignettes:
-shell("rm extras/LocalControl.pdf")
-shell("R CMD Rd2pdf ./ --output=extras/LocalControl.pdf")
